8d66d53a895137d1f8424fe53d062162ce861e57
judongseok <wnehdtjr5@naver.com>
2021-04-01 15:54:43 +0900

    Initial Commit

---
+++ Shared/MojitoEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojitoEatsApp.swift
+//  Shared
+//
+//  Created by judongseok on 2021/04/01.
+//
+
+import SwiftUI
+
+@main
+struct MojitoEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
