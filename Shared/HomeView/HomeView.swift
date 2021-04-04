07836ab8368c58c703d3428f9bcad1c42aafd44e
judongseok <wnehdtjr5@naver.com>
2021-04-05 01:32:12 +0900

    Add NavigationView

--- Shared/HomeView/HomeView.swift
+++ Shared/HomeView/HomeView.swift
@@ -12,42 +12,46 @@ struct HomeView: View {
     @State var timer = Timer.publish(every: 0.001, on: .main, in: .tracking).autoconnect()
     @State var show = false
     var body: some View {
-        VStack{
-            // navigation
-            NavbarView()
-            ZStack{
-                ScrollView(.vertical, showsIndicators: false, content: {
-                    // events
-                    EventView()
-                    // kategorie
-                    KategorieView()
-                    // popular
-                    PopularView()
-                    // choice_store
-                    ChoiceStore()
-                    GeometryReader{g in
-                        StickyHeader()
-                            .onReceive(self.timer, perform: { _ in
-                                // for tracking the image is scrolled out or not
-                                let y = g.frame(in: .global).minY
-                                // -511
-                                if -y > (UIScreen.main.bounds.height / 2.2) - (UIScreen.main.bounds.height * 0.57){
-                                    self.show = true
-                                }
-                                else{
-                                    self.show = false
-                                }
-                            })
+        NavigationView{
+            VStack{
+                // navigation
+                NavbarView()
+                ZStack{
+                    ScrollView(.vertical, showsIndicators: false, content: {
+                        // events
+                        EventView()
+                        // kategorie
+                        KategorieView()
+                        // popular
+                        PopularView()
+                        // choice_store
+                        ChoiceStore()
+                        GeometryReader{g in
+                            StickyHeader()
+                                .onReceive(self.timer, perform: { _ in
+                                    // for tracking the image is scrolled out or not
+                                    let y = g.frame(in: .global).minY
+                                    // -511
+                                    if -y > (-105){
+                                        self.show = true
+                                    }
+                                    else{
+                                        self.show = false
+                                    }
+                                })
+                        }
+                        Content()
+                            .offset(y: 50)
+                    })
+                    .padding(.vertical, -8)
+                    if self.show{
+                        TopView()
+                            .offset(y: 5)
                     }
-                    Content()
-                        .offset(y: 50)
-                })
-                .padding(.vertical, -8)
-                if self.show{
-                    TopView()
                 }
             }
-        } 
+            .navigationBarHidden(true)
+        }
     }
 }
 
--- Shared/HomeView/PopularView.swift
+++ Shared/HomeView/PopularView.swift
@@ -25,39 +25,50 @@ struct PopularView: View {
             ScrollView (.horizontal, showsIndicators: false) {
                 HStack{
                     ForEach(foods, id: \.self){ food in
-                        VStack(alignment: .leading){
-                            Image(food)
-                                .resizable()
-                                .frame(width: 140, height: 140)
-                                .padding(.init(top: 0, leading: -8, bottom: 0, trailing: 3))
-                            Text(food)
-                                .font(.system(size: 18))
-                                .fontWeight(.medium)
-                                .padding(.init(top: 0, leading: -7, bottom: 0, trailing: 0))
-                            HStack{
-                                Image(systemName: "star.fill")
+                        NavigationLink(destination: Text("asdf"), label: {
+                            VStack(alignment: .leading){
+                                Image(food)
                                     .resizable()
-                                    .frame(width: 15, height: 15)
-                                    .foregroundColor(.yellow)
-                                Text("4.5(300) · 0.7km")
+                                    .frame(width: 140, height: 140)
+                                    .padding(.init(top: 0, leading: -8, bottom: 0, trailing: 3))
+                                Text(food)
+                                    .font(.system(size: 18))
+                                    .fontWeight(.medium)
+                                    .padding(.init(top: 0, leading: -7, bottom: 0, trailing: 0))
+                                HStack{
+                                    Image(systemName: "star.fill")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                        .foregroundColor(.yellow)
+                                    Text("4.5(300) · 0.7km")
+                                        .font(.system(size: 14))
+                                        .padding(.init(top: 0, leading: -5, bottom: 0, trailing: 0))
+                                }
+                                .padding(.init(top: -5, leading: -8, bottom: 0, trailing: 3))
+                                
+                                Text("무료배달")
                                     .font(.system(size: 14))
-                                    .padding(.init(top: 0, leading: -5, bottom: 0, trailing: 0))
                             }
-                            .padding(.init(top: -5, leading: -8, bottom: 0, trailing: 3))
-                            
-                            Text("무료배달")
-                                .font(.system(size: 14))
-                        }
-                        .padding(.leading, 8)
+                            .padding(.leading, 8)
+                        })
+                        .buttonStyle(ClearButton(color: Color.black))
                     }
                 }
                 .padding(.leading, 23)
             }
+            Rectangle()
+                .fill(Color(hue: 1.0, saturation: 0.0, brightness: 0.931))
+                .frame(height: 10)
+                .edgesIgnoringSafeArea(.horizontal)
+                .padding(.init(top: 13, leading: 0, bottom: 0, trailing: 0))
         }
-        Rectangle()
-            .fill(Color(hue: 1.0, saturation: 0.0, brightness: 0.931))
-            .frame(height: 10)
-            .edgesIgnoringSafeArea(.horizontal)
-            .padding(.init(top: 13, leading: 0, bottom: 0, trailing: 0))
     }
 }
+
+
+struct PopularView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopularView()
+    }
+}
+
